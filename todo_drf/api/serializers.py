e29b0c720d9a1c800b8d8c7d3bdd22071e7bc673
tticjswo <tticjswo@naver.com>
2022-04-12 16:09:44 +0900

    no message

---
+++ todo_drf/api/serializers.py
@@ -0,0 +1,8 @@
+from dataclasses import field
+from rest_framework import serializers
+from . models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Task
+        fields = '__all__'
